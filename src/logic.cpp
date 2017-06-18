0dc19b129753f3c9f84cb5b7d4c4d2d112bf78be
Barney Wilks <barneywilks1@gmail.com>
2017-06-18 14:53:17 +0100

    Remove travis.yml

--- src/logic.cpp
+++ src/logic.cpp
@@ -24,6 +24,7 @@ void Boot()
         constexpr int period = 200;
         float f = std::cos(Sys::FrameCounter() % period / float(period) * pi<float>() * 2) * 0.5 + 0.5;
         glClearColor(f, f/2, 1 - f, 1);
+
         Sys::EndFrame();
     }
 
